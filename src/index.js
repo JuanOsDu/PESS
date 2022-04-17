5de2688dabdc5bb2a443046fe429e225060a0559
Juan Ospina <juan0806lv@gmail.com>
2022-04-17 16:12:31 -0500

    subida de estrucura para el api de empleados

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const app = express();
 app.use(express.json());
 
 
+
+app.use('/api', require('./routes/empleados.routes'));
+
 app.listen(3000, ()=>{
     console.log(`listen on 3000`);
 })
---
+++ src/models/empleados.shema.js
@@ -0,0 +1,21 @@
+
+const mongoose = require('mongoose');
+
+
+const empleadoShema = mongoose.Schema();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = mongoose.model('Empleado', empleadoShema);
