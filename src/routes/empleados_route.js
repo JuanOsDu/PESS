47c5e6bf1626b59bb1918b0f86175fe234be4a68
DanielC32 <40327371+DanielC32@users.noreply.github.com>
2022-04-25 17:59:43 -0500

    Merge con antiguo empleados api

--- src/routes/empleados_route.js
+++ src/routes/empleados_route.js
@@ -70,3 +70,53 @@ router.get("/calcular-nomina/:id_nomina/:id_empleado", async (req, res) => {
         })
     }
 })
+router.post("/empleado", (req, res) => {
+    const employee = empleadoSchema(req.body);
+    employee
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/empleado", (req, res) => {
+    empleadoSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/empleado/:id", (req, res) => {
+    const { id } = req.params;
+    empleadoSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.put("/empleado/:id", async (req, res) => {
+    const { id } = req.params;
+    const nomina = nominaSchema(req.body);
+    var idNomina = null;
+
+    const consultaNomina = await nominaSchema.findOne({ numero_cheque: req.body.numero_cheque });
+    if (!consultaNomina) {
+        await nomina.save().then((dataNomina) => {
+            idNomina = dataNomina._id;
+        });
+    } else {
+        idNomina = consultaNomina._id;
+        empleadoSchema
+            .updateOne({ _id: id }, {
+                $addToSet: { nominas: idNomina }
+            })
+            .then((data) => res.json(data))
+            .catch((error) => res.json({ message: error }));
+    }
+});
+router.delete("/empleado/:id", (req, res) => {
+    const { id } = req.params;
+    empleadoSchema
+        .remove({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+module.exports = router;
